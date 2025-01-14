d0b65d7f8240a9d0f57a547a1fcd323b3e17016f
Christophe Schmitz <cschmitz@redhat.com>
2025-01-14 11:42:32 +1100

    fix

--- redo-cluster-go.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -rf cluster-go
-cp -r ../hub/cluster-go ./
-echo "REMEMBER I AM HAR DOCING SOME VARIABLE IN the argo-app/*.yaml, such as sno, hub, cszevaco.com, and may need readjustement"
