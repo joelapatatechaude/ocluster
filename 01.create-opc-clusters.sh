38dbc4f79ddc1fe47777a0932048f7d860fe5daf
Christophe Schmitz <cschmitz@redhat.com>
2025-01-14 13:29:08 +1100

    make it aws compatible?

--- 01.create-opc-clusters.sh
+++ 01.create-opc-clusters.sh
@@ -11,3 +11,35 @@ if [ $CLUSTER_TYPE == "bm" ];
 else
     echo "going to run the script"
 fi
+#!/bin/sh
+# NOTES: Attempt to simplify everything.
+# NOTES: Wondering if I can change the domain name to go under cszevaco.
+
+source ~/.secrets/pullsecret
+source ~/.secrets/$MYDIR/route53
+
+function cluster {
+    date
+    cp install-config.yaml.template ./install-config.yaml
+    sed -i "s/DOMAIN/$ROUTE53_DOMAIN/g" ./install-config.yaml
+    sed -i "s/REGION/$REGION/g" ./install-config.yaml
+    sed -i "s/CLUSTER_NAME/$CLUSTER_NAME/g" ./install-config.yaml
+    sed -i "s/PULL_SECRET/$PULL_SECRET/g" ./install-config.yaml
+    sed -i "s/MASTER_INSTANCE_TYPE/$MASTER_INSTANCE_TYPE/g" ./install-config.yaml
+    sed -i "s/WORKER_INSTANCE_TYPE/$WORKER_INSTANCE_TYPE/g" ./install-config.yaml
+    #openshift-install create cluster
+}
+
+function trust {
+    A=$(oc get configmap/kube-root-ca.crt -n openshift-service-ca -o jsonpath='{.data.ca\.crt}')
+    echo "$A" | sudo tee /etc/pki/ca-trust/source/anchors/$(uuidgen)-$CLUSTER_NAME-OCPDEMO-ca.crt > /dev/null
+    sudo update-ca-trust
+    echo "done updating ca, you might need to restart your browser"
+}
+
+
+cluster
+
+echo "skipping updating ca for now, fix code"
+#KUBECONFIG=$MYDIR/auth/kubeconfig trust
+#echo "done updating ca, you might need to restart your browser"
