319880ade825cc0da436e3d50d69bf2a0a54b491
Christophe Schmitz <cschmitz@redhat.com>
2025-01-14 11:56:21 +1100

    get cluster inf

--- get-cluster-info.sh
+++ get-cluster-info.sh
@@ -1,2 +1,24 @@
 #!/bin/sh
-curl -s https://raw.githubusercontent.com/joelapatatechaude/common-scripts/main/OCP-Demo/get-cluster-info.sh | bash -s -- "$@"
+
+source ./env.sh
+source ~/.secrets/$MYDIR/route53
+
+
+echo "Dashboard: https://console-openshift-console.apps.$CLUSTER_NAME.$ROUTE53_DOMAIN"
+echo "Username:  kubeadmin"
+echo "Password:  $(cat ~/.secrets/$MYDIR/kubeadmin-password)"
+
+exit
+
+
+function get_cluster_info {
+    if [ $CLUSTER_NAME == "crc" ];
+    then
+	aa=https://console-openshift-console.apps-crc.testing/
+    else
+	aa=$(grep "Access the OpenShift web-console here:" ./$MYDIR/.openshift_install.log | tail -1 | awk -F 'web-console here: ' '{print $2}' )
+    fi
+    echo "Dashboard: ${aa::-1}"
+    echo "Username : kubeadmin"
+    echo "Password : $(cat $MYDIR/auth/kubeadmin-password)"
+}
