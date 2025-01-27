77fe135be2784dc10d83274df68240727c7cb6a8
Christophe Schmitz <cschmitz@redhat.com>
2025-01-28 10:12:40 +1100

    add clean file for aws

---
+++ clean-file-aws-instalcluster.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm .openshift_install.log
+rm .openshift_install_state.json
+rm -rf auth
+rm -rf tls
+rm metadata.json
+rm terraform.tfvars.json
+rm terraform.platform.auto.tfvars.json
+rm -rf .clusterapi_output
