8b4b48a77f56f4f5b04352b9918df85f2782caa2
Christophe Schmitz <cschmitz@redhat.com>
2025-01-28 10:26:19 +1100

    add a barrier to 03 script to see how to fix an issue I have

--- 03.argo-setup.sh
+++ 03.argo-setup.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 source ./env.sh
 
+echo "Trying to get to the bottom of an issue, try this:"
+echo "First login int argocd with the argocd admin user/password"
+echo "run this script"
+echo "If it works (If I don't get some authorization issue when deploying an argo-app), then see if I can automate that bit"
+echo "If it doesnt work, keep investigating"
+echo "remember, the issue, is that usually, I need to run this script a second time at a later stage, essentailly when I see the authorization messages in argocd interface"
+echo "exiting"
+exit
+
+
 
 function patch_label {
     KUBECONFIG=$KUBECONFIG oc patch argocds openshift-gitops -n openshift-gitops --type='merge' -p '{"spec":{"applicationInstanceLabelKey":"argocd.argoproj.io/instance"}}'
