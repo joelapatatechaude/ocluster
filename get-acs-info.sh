abe7ffa76006034c72c09057f18f8b00bd9aec04
Christophe Schmitz <cschmitz@redhat.com>
2025-02-10 11:16:44 +1100

    fix

--- get-acs-info.sh
+++ get-acs-info.sh
@@ -3,28 +3,10 @@
 source ./env.sh
 source ~/.secrets/$MYDIR/route53
 
-NS=acs-instance
-
-#echo "Dashboard: https://console-openshift-console.apps.$CLUSTER_NAME.$ROUTE53_DOMAIN"
-#echo "Username:  kubeadmin"
-#echo "Password:  $(cat ~/.secrets/$MYDIR/kubeadmin-password)"
+NS=stackrox
 
 
 PASS=$(KUBECONFIG=$KUBECONFIG oc -n $NS get secret central-htpasswd -o go-template='{{index .data "password" | base64decode}}')
-echo "Username : admin"
-echo "Password : $PASS"
-
-exit
-
-
-function get_cluster_info {
-    if [ $CLUSTER_NAME == "crc" ];
-    then
-	aa=https://console-openshift-console.apps-crc.testing/
-    else
-	aa=$(grep "Access the OpenShift web-console here:" ./$MYDIR/.openshift_install.log | tail -1 | awk -F 'web-console here: ' '{print $2}' )
-    fi
-    echo "Dashboard: ${aa::-1}"
-    echo "Username : kubeadmin"
-    echo "Password : $(cat $MYDIR/auth/kubeadmin-password)"
-}
+echo "URL : https://central-$NS.apps.$CLUSTER_NAME.$ROUTE53_DOMAIN"
+echo "User: admin"
+echo "Pwd : $PASS"
