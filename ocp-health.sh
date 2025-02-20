8d8105d09b675616fd468e2a872c4e01b39b7f86
Christophe Schmitz <cschmitz@redhat.com>
2025-02-21 09:28:04 +1100

    ocp-heath

--- ocp-health.sh
+++ ocp-health.sh
@@ -6,8 +6,19 @@ echo "Need to be keep working. Essentially, the current curl respond ok"
 echo "if it's up and reunning. Need to check what happens when not ready"
 echo "then write the loop"
 
-while true; do
-    curl  https://api.$CLUSTER_NAME.$ROUTE53_DOMAIN:6443/livez
-    curl  https://api.$CLUSTER_NAME.$ROUTE53_DOMAIN:6443/readyz
-    sleep 5;
-done
+function test {
+    echo "testing $1"
+    while true; do
+	echo "testing..."
+	ANSWER=$(curl -k -s https://api.$CLUSTER_NAME.$ROUTE53_DOMAIN:6443/$1)
+	echo $ANSWER
+	if [ "$ANSWER" == "ok" ]
+	then
+	    break
+	fi
+	sleep 5;
+    done
+}
+
+test livez
+test readyz
