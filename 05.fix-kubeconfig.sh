a6091c26977a6f615aba9aaae6baeaf31c70e5a2
Christophe Schmitz <cschmitz@redhat.com>
2025-05-14 11:27:31 +1000

    fix

--- 05.fix-kubeconfig.sh
+++ 05.fix-kubeconfig.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+echo "After changing my certificate (so not on AWS"
 echo "need to comment all the \"certificate-authority-data\" of my kubeconfig"
 echo "after that, it should work I think"
