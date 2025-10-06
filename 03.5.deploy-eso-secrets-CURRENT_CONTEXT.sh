a854626900da5459dac8e362181d41e1560aa3f5
Christophe Schmitz <cschmitz@redhat.com>
2025-10-06 19:33:58 +1100

    fix

---
+++ 03.5.deploy-eso-secrets-CURRENT_CONTEXT.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#source ./env.sh
+source ~/.secrets/akeyless-ocp-hub.txt
+
+
+oc create -f - <<EOF
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: external-secrets
+  labels:
+    argocd.argoproj.io/managed-by: openshift-gitops
+EOF
+
+sleep 1
+### THE API AKEYLESS CREDS
+## THIS SECRET WILL BE REFERENCED BY THE clustersecretstore to be created later
+oc create -f - <<EOF
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: akeyless-secret-creds
+  namespace: external-secrets
+type: Opaque
+stringData:
+  accessId: $AKEYLESS_ACCESS_ID
+  accessType:  api_key
+  accessTypeParam:  $AKEYLESS_ACCESS_KEY
+EOF
+
+echo "done creating 1 secret for the ESO to access akeyless"
--- aws-stop-instances.sh
+++ aws-stop-instances.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "trying to avoid empty string issue, check the code and execution, then remove me"
 source ./env.sh
 set -a
 source ~/.secrets/$MYDIR/aws-creds
@@ -8,6 +9,10 @@ set +a
 IDS=$(./oc get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.spec.providerID}{"\n"}{end}' | awk -F '///' '{print $2}' | awk -F '/' '{print $2}')
 echo $IDS
 
-
+if [ -z "${IDS}" ];
+then
+    echo "string empty, nothing to step, exit"
+    exit
+fi
 echo "$IDS" > aws-instances-to-resume.txt
 aws ec2 stop-instances --instance-ids $IDS --region $REGION
