15a1edce788950c214eb87c8247328c77578c7a7
Christophe Schmitz <cschmitz@redhat.com>
2025-11-05 13:13:23 +1100

    fix

--- terra-ai/terra.tf
+++ terra-ai/terra.tf
@@ -46,6 +46,18 @@ variable "ami_id" {
   default     = ""
 }
 
+variable "openshift_cluster_name" {
+  description = "OpenShift cluster name"
+  type        = string
+  default     = "my-cluster"
+}
+
+variable "worker_ignition_url" {
+  description = "URL to worker ignition config (optional - can be added later)"
+  type        = string
+  default     = ""
+}
+
 # Data source for specific Fedora AMI by name
 data "aws_ami" "fedora" {
   most_recent = true
@@ -54,6 +66,22 @@ data "aws_ami" "fedora" {
   filter {
     name   = "name"
     values = ["*Fedora-Cloud-Base-AmazonEC2.x86_64-43-1*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Data source for RHEL CoreOS AMI
+data "aws_ami" "rhcos" {
+  most_recent = true
+  owners      = ["531415883065"] # RHCOS owner ID
+
+  filter {
+    name   = "name"
+    values = ["rhcos-418*x86_64*"]
   }
 
   filter {
@@ -231,6 +259,35 @@ resource "aws_security_group" "private_instance" {
   }
 }
 
+# Security Group for OpenShift Worker
+resource "aws_security_group" "openshift_worker" {
+  name        = "openshift-worker-sg"
+  description = "Security group for OpenShift worker node"
+  vpc_id      = aws_vpc.main.id
+
+  # Allow all traffic from within VPC
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+    description = "All traffic from VPC"
+  }
+
+  # Allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "All outbound traffic"
+  }
+
+  tags = {
+    Name = "openshift-worker-sg"
+  }
+}
+
 # EC2 Instance 1 - Tailscale Router
 resource "aws_instance" "tailscale_router" {
   ami                    = data.aws_ami.fedora.id
@@ -277,8 +334,8 @@ resource "aws_instance" "private_instance" {
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.private_instance.id]
   
-  # Disable public IP assignment
-  associate_public_ip_address = false
+  # Enable public IP for outbound internet access
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
@@ -289,6 +346,84 @@ resource "aws_instance" "private_instance" {
   tags = {
     Name = "private-instance"
     Role = "private"
+  }
+}
+
+data "local_file" "discovery_ign" {
+  filename = "${path.module}/discovery.ign"
+}
+
+# EC2 Instance 3 - OpenShift Worker Node
+resource "aws_instance" "openshift_worker" {
+  ami                    = data.aws_ami.rhcos.id
+  instance_type          = "m6i.xlarge"
+  key_name               = aws_key_pair.deployer.key_name
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.openshift_worker.id]
+
+  user_data                   = data.local_file.discovery_ign.content
+  user_data_replace_on_change = true
+  
+  # Enable public IP for outbound internet access
+  associate_public_ip_address = true
+
+  # Larger root volume for OpenShift
+  root_block_device {
+    volume_size = 300
+    volume_type = "gp3"
+    encrypted   = true
+  }
+
+  # User data for ignition - will be configured after instance creation
+  # For now, we'll use a basic script to set up routing
+  user_data = base64encode(jsonencode({
+    ignition = {
+      version = "3.2.0"
+    }
+    storage = {
+      files = [
+        {
+          path = "/etc/sysconfig/network-scripts/route-eth0"
+          mode = 420
+          contents = {
+            source = "data:,10.76.18.0/23%20via%20${aws_instance.tailscale_router.private_ip}"
+          }
+        }
+      ]
+    }
+    systemd = {
+      units = [
+        {
+          name    = "configure-routes.service"
+          enabled = true
+          contents = <<-EOF
+            [Unit]
+            Description=Configure static route to OpenShift cluster
+            After=network-online.target
+            Wants=network-online.target
+            
+            [Service]
+            Type=oneshot
+            ExecStart=/usr/sbin/ip route add 10.76.18.0/23 via ${aws_instance.tailscale_router.private_ip}
+            RemainAfterExit=yes
+            
+            [Install]
+            WantedBy=multi-user.target
+          EOF
+        }
+      ]
+    }
+  }))
+
+  tags = {
+    Name                                              = "openshift-worker-${var.openshift_cluster_name}"
+    Role                                              = "worker"
+    "kubernetes.io/cluster/${var.openshift_cluster_name}" = "owned"
+  }
+
+  # Prevent accidental deletion
+  lifecycle {
+    ignore_changes = [user_data]
   }
 }
 
@@ -304,8 +439,8 @@ output "tailscale_router_private_ip" {
 }
 
 output "private_instance_public_ip" {
-  description = "Public IP of private instance (none - access via Tailscale)"
-  value       = "N/A - No public IP"
+  description = "Public IP of private instance (for outbound internet access only)"
+  value       = aws_instance.private_instance.public_ip
 }
 
 output "private_instance_private_ip" {
@@ -326,4 +461,24 @@ output "fedora_ami_id" {
 output "fedora_ami_name" {
   description = "Fedora AMI name used"
   value       = data.aws_ami.fedora.name
+}
+
+output "openshift_worker_public_ip" {
+  description = "Public IP of OpenShift worker node"
+  value       = aws_instance.openshift_worker.public_ip
+}
+
+output "openshift_worker_private_ip" {
+  description = "Private IP of OpenShift worker node"
+  value       = aws_instance.openshift_worker.private_ip
+}
+
+output "rhcos_ami_id" {
+  description = "RHEL CoreOS AMI ID used"
+  value       = data.aws_ami.rhcos.id
+}
+
+output "rhcos_ami_name" {
+  description = "RHEL CoreOS AMI name used"
+  value       = data.aws_ami.rhcos.name
 }
