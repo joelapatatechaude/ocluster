f73a38036bbfda51b950ef89b49ed1284be9d0b8
Christophe Schmitz <cschmitz@redhat.com>
2025-12-19 20:41:46 +1100

    add portworx stuff

---
+++ relogin-global-oc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+source ./env.sh
+source ~/.secrets/$MYDIR/route53
+set -eux
+#echo "Relogin disabled at the moment in the sh script"
+#exit
+
+#KUBECONFIG=$KUBECONFIG oc login -u kubeadmin -p `cat ~/.secrets/$MYDIR/kubeadmin-password`  https://api.$CLUSTER_NAME.$ROUTE53_DOMAIN:6443
+
+
+oc login $(./oc whoami --show-server) --token=$(./oc whoami --show-token)
