38dd871806bd589a8fd6a26a07f77f483e0154db
Christophe Schmitz <cschmitz@rechat.com>
2026-06-25 09:29:16 +1000

    update

--- relogin.sh
+++ relogin.sh
@@ -6,4 +6,4 @@ source ~/.secrets/$MYDIR/route53
 #echo "Relogin disabled at the moment in the sh script"
 #exit
 
-KUBECONFIG=$KUBECONFIG oc login -u kubeadmin -p `cat ~/.secrets/$MYDIR/kubeadmin-password`  https://api.$CLUSTER_NAME.$ROUTE53_DOMAIN:6443
+KUBECONFIG=$KUBECONFIG oc login -u `cat ~/.secrets/$MYDIR/admin-username` -p `cat ~/.secrets/$MYDIR/admin-password`  https://api.$CLUSTER_NAME.$ROUTE53_DOMAIN:6443
