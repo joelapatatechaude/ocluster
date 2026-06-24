b656aebfa92aaa5cb18a0030a1640bf9ebbafa03
Christophe Schmitz <cschmitz@rechat.com>
2026-06-24 11:08:26 +1000

    fix

--- vnc.sh
+++ vnc.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 source `dirname $0`/env.sh
-#set -eux
+set -eux
 echo "use it with namespace and vm name:"
-echo "./$0 my-vm -n mynamespace"
+echo "./$0 my-vm mynamespace port"
+
+vm=$1
+ns=$2
+port=$3
+
+echo $vm
+echo $ns
+echo $port
 
 
 
-KUBECONFIG=$KUBECONFIG virtctl vnc $@ --port 5901 --proxy-only &
-remmina -c vnc://localhost:5901
-REMMPID=$!
-echo "test"
-echo $REMMPID
+KUBECONFIG=$KUBECONFIG virtctl vnc $vm -n $ns --port $port --proxy-only &
+remmina -c vnc://$vm.localhost:$port
 
 wait
